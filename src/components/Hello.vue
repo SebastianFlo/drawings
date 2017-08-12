b92413c0355aa810426c06cfc1069cabe0d9b438
Sebastian Florian <rebardesigndk@gmail.com>
2017-08-12 14:14:55 +0200

    First commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <md-card>
+      <md-card-media>
+        <img src="../assets/logo.png"" alt="People">
+      </md-card-media>
+
+      <md-card-header>
+        <div class="md-title">Title goes here</div>
+        <div class="md-subhead">Subtitle here</div>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+
+      <md-card-content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  components: {
+
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
