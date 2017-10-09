b4cb5e005c9fbb8fb4ee89a4466bfbf7658bd0fa
Sebastian Florian <rebardesigndk@gmail.com>
2017-10-09 13:22:38 +0200

    fix(router): Fix contact component

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '@/pages/Home';
-import Contact from '@/pages/Home';
+import Contact from '@/pages/Contact';
 
 Vue.use(Router);
 
