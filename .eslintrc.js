2ad545b84c5f20c147d71301327172dd2ac852b9
Sebastian Florian <rebardesigndk@gmail.com>
2017-10-22 13:58:36 +0200

    chore(EsLint): Fix rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
       }
     }
   },
+  globals: {
+    'Vue': true
+  },
   // add your custom rules here
   'rules': {
     // don't require .vue extension when importing
@@ -30,17 +33,19 @@ module.exports = {
       'vue': 'never'
     }],
     // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    'comma-dangle': ['off'],
-    'space-before-function-paren': ['off'],
-    'object-shorthand': ['off'],
-    'arrow-parens': ['off'],
-    'no-param-reassign': ['off'],
-    'no-plusplus': ['off'],
-    'indent': ['off'],
-    'prefer-arrow-callback': ['off'],
+    'import/no-extraneous-dependencies': 0,
+    'comma-dangle': 0,
+    'space-before-function-paren': 0,
+    'object-shorthand': 0,
+    'arrow-parens': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    'indent': 0,
+    'prefer-arrow-callback': 0,
+    'no-tabs': 0,
+    'no-new': 0,
+    'no-unused-vars': 1,
+    'no-return-assign': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
