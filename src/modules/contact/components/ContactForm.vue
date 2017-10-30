5809f2a5f6a64f830bc78a25423d2c26679d5067
Sebastian Florian <rebardesigndk@gmail.com>
2017-10-30 13:45:51 +0100

    feature(ContactForm): Add component

---
+++ src/modules/contact/components/ContactForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <md-layout md-align="center" class="flex-vert">
+            {{ greeting }}
+        </md-layout>
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'thea-contact-form',
+        data() {
+            return {
+                greeting: 'Contact Form'
+            };
+        }
+    };
+
+</script>
+
+<!-- Add "scoped " attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
